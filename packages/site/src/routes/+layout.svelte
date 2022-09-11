47ec9317669d4a963b6ea035f922e620af9989e6
Sam Wight <samuelwight@gmail.com>
2022-09-11 11:04:47 -0500

    Move CSS to `layout.svelte` for global CSS.

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import "../styles/global.css";
   import Nav from "$lib/components/Nav.svelte";
   import Seo from "$lib/components/Seo.svelte";
   import VercelBanner from "$lib/components/VercelBanner.svelte";
@@ -11,3 +10,110 @@
 <slot />
 
 <VercelBanner />
+
+<style global>
+  * {
+    font-family: "Inter", sans-serif;
+    box-sizing: border-box;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    background-color: #1b1a1b;
+    color: white;
+    background-image: linear-gradient(#1b1a1b 1px, transparent 1px),
+      linear-gradient(to right, #1b1a1b 1px, #000000 1px);
+    background-size: 20px 20px;
+
+    box-sizing: border-box;
+  }
+
+  a {
+    color: rgb(0, 100, 200);
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  a:visited {
+    color: rgb(0, 80, 160);
+  }
+
+  label {
+    display: block;
+  }
+
+  input,
+  button,
+  select,
+  textarea {
+    font-family: inherit;
+    font-size: inherit;
+    -webkit-padding: 0.4em 0;
+    padding: 0.4em;
+    margin: 0 0 0.5em 0;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+  }
+
+  input:disabled {
+    color: #ccc;
+  }
+
+  button {
+    color: #333;
+    background-color: #f4f4f4;
+    outline: none;
+  }
+
+  button:disabled {
+    color: #999;
+  }
+
+  button:not(:disabled):active {
+    background-color: #ddd;
+  }
+
+  button:focus {
+    border-color: #666;
+  }
+
+  .responsive-container {
+    padding: 0px 20px;
+    width: 100%;
+    max-width: 1000px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .card {
+    background-color: #1b1a1b;
+    padding: 20px;
+    border-radius: 15px;
+    overflow: hidden;
+  }
+
+  .markdown p {
+    line-height: 200%;
+  }
+
+  .markdown a {
+    color: #c70909;
+    text-decoration: underline;
+  }
+
+  .markdown li {
+    line-height: 200%;
+    margin-left: 20px;
+  }
+</style>
