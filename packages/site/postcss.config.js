7f23212f3c5fa1846ac7a0fea3afffb4389f0b25
Sam Wight <samuelwight@gmail.com>
2022-09-12 08:35:02 -0500

    Add postcss, autoprefixer, cssnano, and flexbug fixes.

---
+++ packages/site/postcss.config.js
@@ -0,0 +1,13 @@
+import cssnano from "cssnano";
+import autoprefixer from "autoprefixer";
+import flexbugs from "postcss-flexbugs-fixes";
+
+export default {
+  plugins: [
+    autoprefixer,
+    flexbugs,
+    cssnano({
+      preset: "advanced",
+    }),
+  ],
+};
