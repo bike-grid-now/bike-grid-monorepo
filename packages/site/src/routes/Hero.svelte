b93ff3e9a356a5c61f5d7469fb653f99754e442c
Sam Wight <samuelwight@gmail.com>
2022-09-20 14:21:40 -0500

    Add simple icons, other icon package was getting blocked. :(

--- packages/site/src/routes/Hero.svelte
+++ packages/site/src/routes/Hero.svelte
@@ -2,11 +2,10 @@
   import BackgroundImage from "./BackgroundImage.svelte";
   import headerPhotoUrl from "../static/header-backdrop.jpeg";
 
-  import LocationOn from "@material-symbols/svg-400/rounded/location_on.svg?component";
+  import * as simpleIcons from "simple-icons/icons";
 
   export let title: string;
   export let tagline: string;
-  import SocialIcons from "@rodneylab/svelte-social-icons";
 </script>
 
 <BackgroundImage src={headerPhotoUrl} brightness={0.5} maxWidth={1020}>
@@ -16,29 +15,29 @@
       <p>{tagline}</p>
     </div>
 
-    <!-- <div class="location">
-      <LocationOn
-        viewBox="0 0 48 48"
-        style="width: calc(8 * var(--space)); height: calc(8 * var(--space))"
-      />
-      <p>Chicago, IL</p>
-    </div> -->
-
-    <div class="socials">
+    <div class="links">
       <a href="https://www.twitter.com/bikegridnow" target="_blank">
-        <SocialIcons width={40} height={40} network="twitter" />
+        <div class="icon">
+          {@html simpleIcons.siTwitter.svg}
+        </div>
       </a>
       <a href="https://www.instagram.com/bikegridnow/" target="_blank">
-        <SocialIcons width={40} height={40} network="instagram" />
+        <div class="icon">
+          {@html simpleIcons.siInstagram.svg}
+        </div>
       </a>
       <a href="https://www.facebook.com/BikeGridNow/" target="_blank">
-        <SocialIcons width={40} height={40} network="facebook" />
+        <div class="icon">
+          {@html simpleIcons.siFacebook.svg}
+        </div>
       </a>
       <!-- <a href="https://www.tiktok.com/@chicagobikegridnow" target="_blank">
         <SocialIcons width={40} height={40} network="reddit" />
       </a> -->
       <a href="https://www.tiktok.com/@chicagobikegridnow" target="_blank">
-        <SocialIcons width={40} height={40} network="tiktok" />
+        <div class="icon">
+          {@html simpleIcons.siTiktok.svg}
+        </div>
       </a>
     </div>
   </div>
@@ -71,21 +70,22 @@
     font-size: var(--text-5xl);
   }
 
-  .location {
+  .links {
     display: flex;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
-    color: var(--color-text);
-    fill: currentColor;
+    gap: calc(var(--space) * 4);
   }
 
-  .location > :global(* + *) {
-    margin-left: var(--space);
-  }
-
-  .socials a {
+  .links a {
     color: var(--color-text);
     fill: currentColor;
     text-decoration: none;
+  }
+
+  .links .icon {
+    width: calc(var(--space) * 7);
+    height: 2rem;
   }
 </style>
