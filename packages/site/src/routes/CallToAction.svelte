6eded83440ced2666c3d59961e260f797cc4b8fd
Sam Wight <samuelwight@gmail.com>
2022-09-28 11:37:36 -0500

    Add some CTA stuff.

--- packages/site/src/routes/CallToAction.svelte
+++ packages/site/src/routes/CallToAction.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import MailIcon from "@material-symbols/svg-400/rounded/mail.svg?component";
   import EditNote from "@material-symbols/svg-400/rounded/edit_note.svg?component";
-  import CallIcon from "@material-symbols/svg-400/rounded/call.svg?component";
-  import EditNoteIcon from "@material-symbols/svg-400/rounded/edit_note.svg?component";
   import ChevronRightIcon from "@material-symbols/svg-400/rounded/chevron_right.svg?component";
   import DirectionsBusIcon from "@material-symbols/svg-400/rounded/directions_bus.svg?component";
   import LocationOnIcon from "@material-symbols/svg-400/rounded/location_on.svg?component";
@@ -14,50 +12,45 @@
 </script>
 
 <div class="button-group">
-  <a href="https://newsletter.bikegridnow.org/" target="_blank" class="dark">
+  <a href="https://newsletter.bikegridnow.org/" target="_blank" class="light">
     <div class="content">
-      <MailIcon {...iconProps} />
-      <p>Sign up for the Newsletter</p>
+      <p class="heading">Sign up for the Newsletter</p>
+      <p>Keep up with our latest events and actions.</p>
     </div>
     <ChevronRightIcon {...iconProps} />
   </a>
 
-  <a href="https://www.change.org/p/chicago-needs-a-bike-grid" target="_blank" class="light">
-    <div class="content">
-      <EditNote {...iconProps} />
-      <p>Sign the Petition</p>
-    </div>
-    <ChevronRightIcon {...iconProps} />
-  </a>
-
-  <!-- <a
-    href="https://docs.google.com/forms/d/e/1FAIpQLSegNjFMyIcrGiEKp5Y7RQFUjcu4Mk-gfllJrEC13NFk6SGxvw/viewform?usp=sf_link"
+  <a
+    href="https://www.change.org/p/chicago-needs-a-bike-grid"
     target="_blank"
-    class="dark"
+    class="light"
   >
     <div class="content">
-      <DirectionsBusIcon {...iconProps} />
-      <p>Request a Bike Bus</p>
+      <p class="heading">Sign the Petition</p>
+      <p>Sign our petition asking city hall for a bike grid.</p>
     </div>
     <ChevronRightIcon {...iconProps} />
-  </a> -->
+  </a>
 
   <a
     href="https://docs.google.com/forms/d/e/1FAIpQLScmo18WuNDZXnMfEdpVi6NErKp4fvCFHEe6x7GaE0ok1tvDIA/viewform"
     target="_blank"
-    class="dark"
+    class="light"
   >
     <div class="content">
-      <DirectionsBusIcon {...iconProps} />
-      <p>Chicago Mobility Collaborative</p>
+      <p class="heading">Chicago Mobility Collaborative</p>
+      <p>
+        Help us send a strong message to CDOT and RSVP for the Chicago Mobility
+        Collaborative meeting.
+      </p>
     </div>
     <ChevronRightIcon {...iconProps} />
   </a>
 
   <a href="https://bikebustracker.bikegridnow.org/" class="light">
     <div class="content">
-      <LocationOnIcon {...iconProps} />
-      <p>Track the Bike Bus</p>
+      <p class="heading">Track the Bike Bus</p>
+      <p>See our bike bus routes and follow the bike bus in real-time.</p>
     </div>
     <ChevronRightIcon {...iconProps} />
   </a>
@@ -72,33 +65,38 @@
 
     background-color: var(--color-primary);
     padding: calc(6 * var(--space));
-    font-size: var(--text-2xl);
     cursor: pointer;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    border-radius: 15px;
+
+    transition: all 200ms ease-in-out;
+  }
+
+  a:hover,
+  a:focus {
+    background-color: var(--color-primary);
   }
 
   .button-group {
-    border-radius: 15px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: calc(var(--space) * 3);
     overflow: hidden;
-    border: 1px solid var(--color-border);
-    box-shadow: 0 2px 3px var(--color-shadow);
-  }
-
-  .dark {
-    background-color: var(--color-card);
   }
 
   .light {
     background-color: var(--color-card-light);
   }
 
-  .content {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    gap: calc(2 * var(--space));
+  .heading {
+    font-size: var(--text-2xl);
+    margin-bottom: calc(var(--space) * 2);
+    font-weight: 500;
+  }
+
+  p {
+    line-height: 1.4;
   }
 </style>
