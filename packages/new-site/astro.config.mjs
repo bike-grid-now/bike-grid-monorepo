95410d5986e58c7ca4efc771276fd499265f1572
Sam Wight <me@samw.dev>
2023-01-27 10:35:44 -0600

    Enable Astro SSR.

--- packages/new-site/astro.config.mjs
+++ packages/new-site/astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import svelteSvg from "vite-plugin-svelte-svg";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   vite: {
-    plugins: [svelteSvg()],
+    plugins: [svelteSvg()]
   },
+  output: "server",
+  adapter: vercel()
 });
