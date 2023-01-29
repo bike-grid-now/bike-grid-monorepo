1aa1cd1b6df457f68a5274b9bf6e8f34317b7d7c
Sam Wight <me@samw.dev>
2023-01-29 10:37:01 -0600

    Convert Nav to Tailwind.

--- packages/new-site/astro.config.mjs
+++ packages/new-site/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import svelteSvg from "vite-plugin-svelte-svg";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), tailwind()],
   vite: {
     plugins: [svelteSvg()]
   },
---
+++ packages/new-site/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,js,jsx,svelte,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
